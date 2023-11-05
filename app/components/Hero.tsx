aaba89ac25b667393b68c41f9e549702a17bda12
Bhanu1776 <sunkabhanu281202@gmail.com>
2023-11-05 20:44:14 +0530

    fix(Hero): Fixed performance issues noticed on lighthouse

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -13,11 +13,11 @@ const Hero = () => (
       Build your audience and grow your <br className="hidden lg:block" />
       <span>brand</span>
     </h1>
-    <h3 className="my-8 text-center font-light xxsm:px-4 xxsm:text-lg md:text-xl lg:p-0">
+    <p className="my-8 text-center font-light xxsm:px-4 xxsm:text-lg md:text-xl lg:p-0">
       no more, no less. Deploy from our single pane of glass, manage them with
       ease and scale up as <br className="hidden lg:block" />
       <span>fast as your workload grows </span>
-    </h3>
+    </p>
     <button
       type="button"
       className="rounded-md bg-blue-700 px-8 py-2 text-lg font-semibold text-white md:mt-5 md:px-10 md:py-3"
@@ -25,7 +25,6 @@ const Hero = () => (
       Get Started Now
     </button>
     <Image
-      loading="lazy"
       src="/Assets/Hero.png"
       alt="Architecture"
       className="mt-5 xxsm:h-52 xxsm:px-4 md:h-auto md:max-w-full lg:px-0"
